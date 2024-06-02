97a132e1f32e31ce883e0c12b22bdebcffa67cc9
Aditya Mewada <eng.adityamewada@gmail.com>
2024-06-02 16:00:41 +0530

    leetcode

---
+++ leetcode/array/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.array;
+
+class Solution {
+    public int scoreOfString(String s){
+        int sum=0;
+        for(int i =0;i<s.length();i++){
+         sum =  sum + Math.abs((int)s.charAt(i) - (int)s.charAt(i+1));
+        }
+        return sum;
+    }
