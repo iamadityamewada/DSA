6afa8652f8fc8b4a1660fef4e1a6d610a37866ef
Aditya Mewada <eng.adityamewada@gmail.com>
2024-10-08 17:44:26 +0530

    updates

---
+++ techsimPy/basic.py
@@ -0,0 +1,28 @@
+# print("Input Function")
+# num1 = int(input("enter num1: "))
+# num2 = int(input("enter num2: "))
+# print(num1 + num2)
+
+# num1 = int(input("enter num1: "))
+# name = input("enter your name: ")
+# # for _ in range(0 , num1):
+# #     print(name)
+
+# print((name + "\t") *num1)
+
+# cgpa = float(input("Enter your CGPA: "))
+# print(f"Your Percentage is {cgpa*10}%")
+# print("Your Pecentage is", str(cgpa*10)+"%")
+
+# fname = input("Enter your first name:")
+# lname = input("Enter your last name:")
+# add = input("enter your address :")
+# college = input("enter your college name:")
+# print(f"Hello!, my name is {fname} {lname}, i am from {add}, i have completed my graduation from {college}")
+
+msg = input("enter your msg:")
+num = int(input("enter no:"))
+
+msg = msg[num:]
+print(msg)
+
