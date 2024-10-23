14902263a9b44c00514f43b1d08824994eae0c52
Aditya Mewada <eng.adityamewada@gmail.com>
2024-10-23 22:15:18 +0530

    update

--- techsimPy/condition.py
+++ techsimPy/condition.py
@@ -21,7 +21,49 @@
 # elif color.lower()== "green":
 #     print("GO")
 # else:
-#     print("Enter valid color")            
+#     print("Enter valid color")    
+# 
+# # number = int(input("enter the nu.:- "))
+# if number % 2 == 0 :
+#     print("if it's even")
+# elif number % 3 == 0:
+#     print("if it's odd")
+
+# day = (input("enter the day:- "))
+# if day == "sunday":
+#     print("relax day")
+# elif day == "saturday":
+#     print("weekend day")
+# elif day == "monday"or day =="tuesday" or day == "wednesday" or day =="thuresday" or day =="friday":
+#     print("study day")
+# else:
+# #     print("invalid entry")
+
+# data = input("Enter any value:- ")
+# if data.isalpha():
+#     print("char")
+#     vowels = ["a","e","i","o","u"]
+#     if data.lower() in vowels:
+#         print("vowels")
+#     else:
+#         print("consonent")
+# elif data.isdigit():
+#     print("digit")
+# elif data.isspace():
+#     print("space")
+# else:
+#     print("special char.") ## registration = 0/1
+
+# age = int(input("Enter your age:- "))
+# registration = input("Enter registration 0/1:- ")
+# if age >= 18:
+#     print("eligible for votig")
+#     if registration == "0":
+#         print("your registration")
+#     else:
+#        print("your not registration")
+# if age < 18:
+#     print("you are not eligible for votig")        
 
 
 
--- techsimPy/loop.py
+++ techsimPy/loop.py
@@ -1,3 +1,104 @@
+# start = int(input("Enter starting point:- "))
+# end = int(input("Enter end point:- "))
+# op=range(start,end+1)
+# print(list(op))
+
+# s = int(input("enter start tablie:-"))
+# b = range(s,s*11,s)
+# print(list(b))
+
+# li = range(1,11)
+# for i in li:
+#     print(i)
+
+# li = [1,2,3,4,53,21,34]
+# for i in li:
+#     print("Hello")
+
+# li=("hello")
+# for i in li:
+#     print(i)
+
+# li=[12,34,56,44,56]
+# for i in li:
+#     print(i)
+
+# li=[12,34,56,44,56]
+# for i in li:
+#     print(i+5)
+
+# for i in range(1,11):
+#     print (i)
+
+# student={
+#     "name": "mahak",
+#     "college" : "lnct",
+#     "age": 21
+# }
+
+# for i in student:
+#     print(student[i])
+
+
+# for i in range(50,101):
+#     if i%2==0 and i%3==0:
+#         print(i)
+
+# data=("abc34yu7823qew")
+# for i in data:
+#     if i.isdigit():
+#      print(i, "- digit")
+#     elif i.isalpha():
+#        print(i, "- char")
+
+# data2=("sdggfhgjbncfgtrsdfiui")
+# vowel=["a","e","i","o","u"]
+# for i in data2:
+#     if  i.lower() in vowel:
+#       print(i,("-vowel"))
+#     else:
+#       print (i,("-consonent"))
+
+
+
+# li=[45,34,54,67,23,87]
+# s=0
+# for i in li:
+#   s=s+i
+# print(s)
+
+# li=[45,34,54,67,23,87]
+# s=0
+# for i in li:
+#  if i%2==0:
+#    s=s+i
+# print(s)
+
+
+# li=[2,3,4,5,6]
+# new=[]
+# for i in li:
+#     s=i*i
+#     new.append(s)
+# print(new)
+
+# msg="class will be over in 10 minutes"
+# new=msg.split()
+# for i in new:
+#     print(i)
+
+
+s="hello"
+new=[]
+li=list(s)
+for i in s:
+    last_char = li.pop()
+    li.insert(0,last_char)
+    ab = "".join(li)
+    new.append(ab)
+print(new)
+
+
 s = "hello"
 li = list(s)
 new =[]
