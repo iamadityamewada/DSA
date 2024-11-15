da8157200c393fe33ed1e5f998e898c1fa8b345d
Aditya Mewada <you@example.com>
2024-11-15 19:17:07 +0530

    update

--- techsimPython/function.py
+++ techsimPython/function.py
@@ -31,18 +31,41 @@
 # factorial(6)    
 # 
 
-def calc(num1, num2,opp):
-    if opp == "+":
-        return num1 + num2
-    elif opp == "-":
-        return num1 - num2
-    elif opp == "*":
-        return num1 * num2
-    elif opp == "/" and num2!=0:
-        return num1 / num2
-    elif opp == "//" and num2 !=0:
-        return num1 // num2
-    else:
-        return("not valid input")
+# def calc(num1, num2,opp):
+#     if opp == "+":
+#         return num1 + num2
+#     elif opp == "-":
+#         return num1 - num2
+#     elif opp == "*":
+#         return num1 * num2
+#     elif opp == "/" and num2!=0:
+#         return num1 / num2
+#     elif opp == "//" and num2 !=0:
+#         return num1 // num2
+#     else:
+#         return("not valid input")
         
-print(calc(0,3,"/"))
+# print(calc(0,3,"/"))
+
+# n number of arguments
+# def Add(*a):
+#     # print(type(a))
+#     return sum(a)
+# print(Add(5,3,7,5))
+
+# keyword args
+
+# def stu_detail(**detail):
+#     print(detail)
+
+# stu_detail(name="Priya", gmail ="r@g.com",age ="6")
+
+# # default argument
+# def def_fun(x,y,z=10):
+#     return x+y+z
+# print(def_fun(4,3))
+
+
+def show_employee(name,salary=15000):
+    print(f"your name is {name} and your salary is {salary}")
+show_employee("Priya")
