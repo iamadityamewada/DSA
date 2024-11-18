d96de3f840e4238dc4847b1d2a028ab7180517e4
Aditya Mewada <eng.adityamewada@gmail.com>
2024-11-18 18:09:44 +0530

    practice

--- techsimPython/tuples.py
+++ techsimPython/tuples.py
@@ -1,9 +1,9 @@
-tup = (3,4,5,2,3,6,)
-print(type(tup))
-# print(tup[2:4])
-# tup[3]=67
+# tup = (3,4,5,2,3,6,)
+# print(type(tup))
+# # print(tup[2:4])
+# # tup[3]=67
 
-t = ()
-print(t)
+# t = ()
+# print(t)
 
 # print(tup)
