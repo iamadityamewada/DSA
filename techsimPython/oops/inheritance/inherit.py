2858e446cf2ef175122133d6387740fe8b3667e9
Aditya Mewada <eng.adityamewada@gmail.com>
2024-11-26 18:21:20 +0530

    oopsupdate

--- techsimPython/oops/inheritance/inherit.py
+++ techsimPython/oops/inheritance/inherit.py
@@ -1,5 +1,6 @@
 class Phone:
     def __init__(self):
+        self.name = "nokia"
         print("Basic phone init")
 
     def calling(self):
@@ -8,9 +9,11 @@ class Phone:
     def text(self):
         print("texting")
 
+
 class Smartphone(Phone):
     def __init__(self):
         print("SmartPhone init")
+        # super().__init__()
 
     def video_call(self):
         print("Video Calling")
@@ -18,15 +21,21 @@ class Smartphone(Phone):
 class Samsung(Smartphone):
     def __init__(self):
         print("Samsung init")
+        super().__init__()
+        Phone.__init__(self)
 
     def biometrics(self):
         print("Add finger print")    
 
 
 sm = Samsung()
-sm.calling()
-sm.video_call()
-sm.biometrics()
+
+
+# sm.calling()
+# sm.video_call()
+# sm.biometrics()
+
+
 # Sp = Smartphone()
 # Sp.calling()
 # # bp = Phone()
