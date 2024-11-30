a8984a453c39c28471e327ca8f9f0c151aa7a44e
Aditya Mewada <eng.adityamewada@gmail.com>
2024-11-30 18:37:22 +0530

    stack and queue

--- Stack/stack.py
+++ Stack/stack.py
@@ -4,10 +4,28 @@ class Stack:
 
     def push(self,item):
         self.stack.append(item)
-        print(f"{item} to stack")   
+        print(f"{item} push to stack")   
 
     def pop(self):
         if len(self.stack) > 0:
-           self.stack.pop()
+           last_item = self.stack.pop()
+           print(last_item)
         else:
             print("stack is empty")
+    
+    def peek(self):
+        print(self.stack[len(self.stack)-1])
+
+    def display_stack(self):
+        print("<-".join(self.stack))    
+
+
+book_stack = Stack()
+book_stack.push("Rework")
+book_stack.push("The lean startup")
+book_stack.push("atomic habbit")
+# book_stack.pop()   
+# book_stack.pop()
+# book_stack.pop()    
+# book_stack.peek()   
+book_stack.display_stack()  
