2a67ce7731d6bafa016575b66dad075b287779d8
Aditya Mewada <eng.adityamewada@gmail.com>
2024-12-09 17:31:10 +0530

    update

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -19,6 +19,32 @@
 # # print(node)
 # print(node["head"]["next"]["next"]["value"])
 
+# class Node:
+#     def __init__(self,value):
+#         self.value = value
+#         self.next = None
+
+# class LinkedList:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+
+#     def append(self,value):
+#         new_node = Node(value)
+#         if self.head == None:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             self.tail.next = new_node
+#             self.tail = new_node    
+
+# ls = LinkedList(12)
+# ls.append(23)
+# ls.append(24)
+
+# print(ls.head.value)
+# print(ls.tail.value)
 
 class Node:
     def __init__(self,value):
@@ -26,31 +52,69 @@ class Node:
         self.next = None
 
 class LinkedList:
-    def __init__(self,value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node   
-     
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def append(self,value):
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
-            self.tail = new_node 
+            self.tail = new_node
         else:
             self.tail.next = new_node
+            self.tail = new_node   
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end = "->")
+            temp = temp.next                     
+        print(temp)
+   
+    # def print_list(self):
+    #     temp = self.head
+    #     while temp is not None:
+    #         if temp.value % 2  == 0 :
+    #           print(temp.value, end = "->")
+    #         temp = temp.next
+                                 
+    #     print(temp)
+
+    def prepend(self,value):
+        new_node = Node(value)   
+        # print(new_node.value, new_node.next) 
+        if self.head == None:
+            self.head = new_node
             self.tail = new_node
+        else:
+            # print(new_node.value, new_node.next)
+            new_node.next = self.head
+            self.head = new_node
 
+    def pop_first(self):
+        if self.head == None:
+            return "Empty List"
+        else:
+            value = self.head.value
+            self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+            return value
+    
 
+ls = LinkedList()
+ls.append(23)
+ls.append(24)
+ls.append(32)
+ls.append(28)
+ls.append(42)
+ls.append(23)
+ls.print_list()
+ls.prepend(11)
+ls.print_list()
+print("poped item:", ls.pop_first())
+ls.print_list()
 
-my_linkedlist = LinkedList(11)
-my_linkedlist.append(12)
-my_linkedlist.append(34)
-my_linkedlist.append(32)
-
-
-# print(my_linkedlist.head.value)
-# print(my_linkedlist.tail.value)
-# print(my_linkedlist.head.next.next.next.value)
-# print(my_linkedlist)   
-
-
+# print(ls.head.value)
+# print(ls.tail.value)
