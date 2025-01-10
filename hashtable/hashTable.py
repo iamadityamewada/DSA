0cce32a5f9f5599ba460b9a728dfa01813b6ce68
Aditya Mewada <you@example.com>
2025-01-10 18:47:57 +0530

    update

--- hashtable/hashTable.py
+++ hashtable/hashTable.py
@@ -14,7 +14,57 @@ class HashTable:
             hash_value += ord(i)
         return hash_value * prime_number % self.size 
 
+    def insert(self,key,value):
+        index = self.generate_hash_value(key)    
+        box = self.hash_table[index]
+        if len(box) > 0:
+            for pair in box:
+             if pair[0] == key:
+                 pair[1] = value
+                 return
+        box.append([key,value])
+
+    def print_table(self):
+        for ind,box in enumerate(self.hash_table):
+           print(f" Index - {ind} {box}")  
+
+    def getValue(self,key):
+        index = self.generate_hash_value(key)
+        box = self.hash_table[index]
+        for pair in box:
+            if pair[0]==key:
+                return pair[1]
+            return "key not found" 
+
+    def getKeys(self):
+        keys = []
+        for box in self.hash_table:
+            for pair in box:
+                keys.append(pair[0])
+        return keys 
+
+    def delete(self,key):
+        index = self.generate_hash_value(key)
+        box = self.hash_table[index]
+        for pair in box:
+            if pair[0]==key:
+                box.remove(pair)
+        print("Pair deleted")                 
+                                        
+
 hashtable = HashTable(10)
-print(hashtable.generate_hash_value("pen"))
+hashtable.insert('student','Aditya')
+hashtable.insert('student2','Ady')
+hashtable.insert('subject','Maths')
+hashtable.insert('Roll No',42)
+hashtable.insert('tstuden','Adiii')
+
+hashtable.print_table()
+# hashtable.getValue("student")
+# print(hashtable.getKeys())
+hashtable.delete('student2')
+hashtable.print_table()
+
+
 
 
