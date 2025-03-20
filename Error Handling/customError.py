e035a1d7f99b28a914632696772b3e3d274c3b0b
Aditya Mewada <you@example.com>
2025-03-20 18:33:14 +0530

    custom error

---
+++ Error Handling/customError.py
@@ -0,0 +1,9 @@
+class Number10Error(Exception):
+    def __init__(self,msg,error_code):
+        # self.msg = msg 
+        super().__init__(self,f"{msg} - {error_code}")
+
+num = int(input("Enter a number : "))
+
+if num == 10:
+    raise Number10Error("you can't enter number 10",501)
